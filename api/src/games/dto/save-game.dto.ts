8c15d37cdc6728886997fa101831efc8f567ce3b
felibuscaglia <felipebbuscaglia@gmail.com>
2024-03-11 08:40:16 -0300

    Creates save game DTO

---
+++ api/src/games/dto/save-game.dto.ts
@@ -0,0 +1,96 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMaxSize,
+  IsArray,
+  IsBoolean,
+  IsDefined,
+  IsEnum,
+  IsInstance,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
+import { GameLink, GameMode, Image, Platform, Subgenre } from 'entities';
+import { GamePricing } from 'games/lib/enums';
+import { IsUniqueInArray } from 'games/validators/is-unique-in-array.validator';
+
+export class SaveGameDto {
+  @IsBoolean()
+  creatorInvolvedInDevelopment: boolean;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(40)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(60)
+  tagline?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(260)
+  description?: string;
+
+  @IsOptional()
+  @IsEnum(GamePricing)
+  pricing?: GamePricing;
+
+  @IsDefined()
+  @IsArray()
+  @ArrayMaxSize(4)
+  @ValidateNested({ each: true })
+  @Type(() => Subgenre)
+  subgenres: Subgenre[];
+
+  @IsDefined()
+  @IsArray()
+  @ArrayMaxSize(11)
+  @ValidateNested({ each: true })
+  @Type(() => GameLink)
+  @IsUniqueInArray('platform', {
+    message: 'Platform must not be repeated within links array',
+  })
+  links: GameLink[];
+
+  @IsOptional()
+  @IsObject()
+  @IsNotEmptyObject()
+  @IsInstance(Image)
+  thumbnail?: Image;
+
+  @IsDefined()
+  @IsArray()
+  @ArrayMaxSize(9)
+  @ValidateNested({ each: true })
+  @Type(() => Image)
+  gallery: Image[];
+
+  @IsDefined()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Platform)
+  platforms: Platform[];
+
+  @IsDefined()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => GameMode)
+  modes: GameMode[];
+
+  @IsOptional()
+  @IsString()
+  @IsUrl()
+  videoUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(520)
+  storyline?: string;
+}
--- api/src/games/lib/enums/game-pricing.enum.ts
+++ api/src/games/lib/enums/game-pricing.enum.ts
@@ -1,5 +1,4 @@
 export enum GamePricing {
   FREE = 'free',
   PAID = 'paid',
-  PAID_WITH_FREE_TRIAL = 'paid-with-free-plan',
 }
