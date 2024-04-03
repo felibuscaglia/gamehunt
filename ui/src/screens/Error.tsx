28880aef0d2f0a7bda350856ec918f38c18df324
felibuscaglia <felipebbuscaglia@gmail.com>
2024-04-03 18:45:49 -0300

    Error screen on admin panel

--- ui/src/screens/Error.tsx
+++ ui/src/screens/Error.tsx
@@ -1,8 +1,23 @@
+import PageHead from "components/PageHead";
+import { UI_PATHS } from "lib/constants";
+import { Link } from "react-router-dom";
+
 interface IProps {
   msg: string;
 }
-const ErrorScreen: React.FC<IProps> = () => {
-  return <main>Error screen!</main>;
+const ErrorScreen: React.FC<IProps> = ({ msg }) => {
+  return (
+    <main>
+      <PageHead />
+      <div
+        className="absolute inset-1/2 w-1/2 flex items-center flex-col gap-4"
+        style={{ transform: "translate(-50%, -50%)" }}
+      >
+        <h2 className="text-4xl font-semibold text-center">{msg}</h2>
+        <Link className="underline text-primary-brand-color" to={UI_PATHS.HOME}>Go back.</Link>
+      </div>
+    </main>
+  );
 };
 
 export default ErrorScreen;
