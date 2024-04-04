3460dad3688fd7d6d8a1d610def525c8ab3658df
felibuscaglia <felipebbuscaglia@gmail.com>
2024-04-04 14:33:09 -0300

    Loading screen on admin panel

--- ui/src/screens/Loading.tsx
+++ ui/src/screens/Loading.tsx
@@ -1,5 +1,12 @@
+import { PRIMARY_BRAND_COLOR } from "lib/constants";
+import { PacmanLoader } from "react-spinners";
+
 const LoadingScreen = () => {
-    return <main>Loading screen!</main>
-}
+  return (
+    <main className="h-screen bg-primary-brand-color-light flex items-center justify-center">
+      <PacmanLoader color={PRIMARY_BRAND_COLOR} />
+    </main>
+  );
+};
 
 export default LoadingScreen;
