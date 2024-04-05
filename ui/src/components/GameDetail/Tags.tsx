ab9a1a5f227c9b24748abd67ff5c77e6f55a5a48
felibuscaglia <felipebbuscaglia@gmail.com>
2024-04-05 08:54:31 -0300

    Remove links from platform and game modes tags in game detail screen

--- ui/src/components/GameDetail/Tags.tsx
+++ ui/src/components/GameDetail/Tags.tsx
@@ -7,6 +7,9 @@ interface IProps<T> {
   baseUrl: string;
 }
 
+const TAG_CLASSNAMES =
+  "bg-primary-brand-color-light py-1 px-3 rounded-full flex items-center justify-center";
+
 const GameDetailTags = <T,>({
   elements = [],
   title,
@@ -16,15 +19,28 @@ const GameDetailTags = <T,>({
   return (
     <section className="flex items-center gap-4">
       <span className="font-semibold w-2/12">{title}: </span>
-      {elements.map((el) => (
-        <Link
-          to={`/${baseUrl}/${el["urlSlug" as keyof T]}`}
-          key={`tag-${title}-${el[displayKey]}`}
-          className="bg-primary-brand-color-light py-1 px-3 rounded-full flex items-center justify-center"
-        >
-          <span className="text-sm whitespace-nowrap">{el[displayKey] as any}</span>
-        </Link>
-      ))}
+      {elements.map((el) =>
+        el["urlSlug" as keyof T] ? (
+          <Link
+            to={`/${baseUrl}/${el["urlSlug" as keyof T]}`}
+            key={`tag-${title}-${el[displayKey]}`}
+            className={TAG_CLASSNAMES}
+          >
+            <span className="text-sm whitespace-nowrap">
+              {el[displayKey] as any}
+            </span>
+          </Link>
+        ) : (
+          <div
+            key={`tag-${title}-${el[displayKey]}`}
+            className={TAG_CLASSNAMES + " cursor-default"}
+          >
+            <span className="text-sm whitespace-nowrap">
+              {el[displayKey] as any}
+            </span>
+          </div>
+        )
+      )}
     </section>
   );
 };
