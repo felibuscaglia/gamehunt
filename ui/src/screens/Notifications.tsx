bc3b63bf47bbe1e2c2b2b1bd9ecf4103cb925dec
felibuscaglia <felipebbuscaglia@gmail.com>
2024-04-15 14:32:43 -0300

    Empty placeholder on notifications screen

--- ui/src/screens/Notifications.tsx
+++ ui/src/screens/Notifications.tsx
@@ -1,3 +1,4 @@
+import { IconPlus } from "@tabler/icons-react";
 import Notification from "components/Notification";
 import AuthGuard from "guards/Auth";
 import { API_PATHS } from "lib/constants";
@@ -13,14 +14,24 @@ const NotificationsScreen = () => {
         <section className="flex-grow bg-primary-brand-color-extra-light">
           <div className="w-10/12 mx-auto py-8">
             <h2 className="text-2xl font-semibold">Notifications</h2>
-            <div className="flex flex-col gap-4 mt-4 w-3/4">
-              {notifications.map((notification) => (
-                <Notification
-                  notification={notification}
-                  key={`notification-${notification.id}`}
-                />
-              ))}
-            </div>
+            {notifications.length ? (
+              <div className="flex flex-col gap-4 mt-4 w-3/4">
+                {notifications.map((notification) => (
+                  <Notification
+                    notification={notification}
+                    key={`notification-${notification.id}`}
+                  />
+                ))}
+              </div>
+            ) : (
+              <div
+                className="flex items-center justify-center gap-2 mt-10 absolute inset-1/2 w-full"
+                style={{ transform: "translate(-50%, -50%)" }}
+              >
+                <IconPlus size={20} />
+                <p className="text-xl font-medium">Nothing here yet...</p>
+              </div>
+            )}
           </div>
         </section>
       )}
