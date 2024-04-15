d315d46f113bc0c4e19f79c1230764a76185a808
felibuscaglia <felipebbuscaglia@gmail.com>
2024-04-15 14:52:03 -0300

    Display empty placeholder on My games screen if no games

--- ui/src/screens/Notifications.tsx
+++ ui/src/screens/Notifications.tsx
@@ -14,24 +14,26 @@ const NotificationsScreen = () => {
         <section className="flex-grow bg-primary-brand-color-extra-light">
           <div className="w-10/12 mx-auto py-8">
             <h2 className="text-2xl font-semibold">Notifications</h2>
-            {notifications.length ? (
-              <div className="flex flex-col gap-4 mt-4 w-3/4">
-                {notifications.map((notification) => (
+            <div className="flex flex-col gap-4 mt-4 w-3/4">
+              {notifications.length ? (
+                notifications.map((notification) => (
                   <Notification
                     notification={notification}
                     key={`notification-${notification.id}`}
                   />
-                ))}
-              </div>
-            ) : (
-              <div
-                className="flex items-center justify-center gap-2 mt-10 absolute inset-1/2 w-full"
-                style={{ transform: "translate(-50%, -50%)" }}
-              >
-                <IconPlus size={20} />
-                <p className="text-xl font-medium">Nothing here yet...</p>
-              </div>
-            )}
+                ))
+              ) : (
+                <div
+                  className="bg-white rounded p-5 w-3/4 text-center"
+                  style={{ boxShadow: "rgba(0, 0, 0, 0.1) 0 1px 2px 0" }}
+                >
+                  <span>
+                    👋 <br />
+                    No notifications yet.
+                  </span>
+                </div>
+              )}
+            </div>
           </div>
         </section>
       )}
--- ui/src/screens/UserGames.tsx
+++ ui/src/screens/UserGames.tsx
@@ -1,23 +1,36 @@
 import ConfirmDeleteDialog from "components/Dialog/ConfirmDeleteDialog";
 import UserGame from "components/UserGame";
 import AuthGuard from "guards/Auth";
-import { API_PATHS } from "lib/constants";
+import { API_PATHS, UI_PATHS } from "lib/constants";
 import { IGame } from "lib/interfaces";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const UserGamesScreen = () => {
   return (
     <AuthGuard<IGame[]> apiPath={API_PATHS.GET_MY_GAMES}>
       {(games) => (
         <main className="w-10/12 mx-auto py-5">
-          <h2 className="text-3xl font-bold col-span-3">My games</h2>
-          <div className="mt-8 flex items-start">
-            <section className="w-full">
-              {games.map((game, i) => (
-                <UserGame game={game} key={`user-game-${i}`} />
-              ))}
-            </section>
-          </div>
+          <h2 className="text-3xl font-bold col-span-3 mb-8">My games</h2>
+          {games.length ? (
+            <div className="flex items-start">
+              <section className="w-full">
+                {games.map((game, i) => (
+                  <UserGame game={game} key={`user-game-${i}`} />
+                ))}
+              </section>
+            </div>
+          ) : (
+            <div className="text-center text-lg inset-1/2 absolute w-full" style={{ transform: 'translate(-50%, -50%)'}}>
+              <p className="mb-4">🤔 Looks like you haven't posted any games yet</p>
+              <Link
+                to={UI_PATHS.SUBMIT_GAME}
+                className="text-sm bg-primary-brand-color text-white font-semibold py-2 px-4 rounded"
+              >
+                Create a new game
+              </Link>
+            </div>
+          )}
         </main>
       )}
     </AuthGuard>
